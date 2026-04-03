3fd043d5c29eca261d78f80c5b2ce456ccaa7360
OlgaStarchenko <o9915530@gmail.com>
2026-04-03 23:54:19 +1000

    In the root component GameContainer.jsx, I created the states currentPlayer to track who is currently playing, isGameEnded to track the end of the game, isDraw to track a draw, and field to track the cells of the playing field.

--- src/components/game/GameContainer.jsx
+++ src/components/game/GameContainer.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import { useState } from "react";
 import GameLayout from "./GameLayout";
 
 export function GameContainer() {
+	const [currentPlayer, setCurrentPlayer] = useState("X");
+	const [isGameEnded, setisGameEnded] = useState(false);
+	const [isDraw, setIsDraw] = useState(false);
+
+	const field = ["", "", "", "", "", "", "", "", ""];
+
 	return <GameLayout />;
 }
