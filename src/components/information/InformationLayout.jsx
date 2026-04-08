451b0f09f978fac5a5a8bff2986c2eec58f9367f
OlgaStarchenko <o9915530@gmail.com>
2026-04-08 10:34:05 +1000

    In the InformationLayout component, I changed the <label> with the player names to a <p> with the game name. In the informationLayout.module.css component, I made changes to the styles.

--- src/components/information/InformationLayout.jsx
+++ src/components/information/InformationLayout.jsx
@@ -3,18 +3,17 @@ import styles from "./informationLayout.module.css";
 export function InformationLayout({ isDraw, currentPlayer, winner }) {
 	return (
 		<>
-			<div className={styles.information__top}>
-				<label className={styles.red__label}>player O</label>
-				<label className={styles.blue__label}>player X</label>
+			<div className={styles.information__title}>
+				<p>TIC TAC TOE</p>
 			</div>
 
-			<div className={styles.information__bottom}>
+			<div className={styles.information__message}>
 				{isDraw ? (
-					<label className={styles.black__text}>Draw</label>
+					<label className={styles.red__text}>Draw</label>
 				) : null}
 
 				{winner && (
-					<label className={styles.black__text}>
+					<label className={styles.red__text}>
 						{`Win player ${currentPlayer}!`}
 					</label>
 				)}
